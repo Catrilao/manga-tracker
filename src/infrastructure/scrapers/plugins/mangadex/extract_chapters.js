cd901a2c578022ce84bb71104b1454e9973216ae
Catrilao <87039539+Catrilao@users.noreply.github.com>
2026-06-29 11:52:26 -0400

    style: format code with prettier

--- src/infrastructure/scrapers/plugins/mangadex/extract_chapters.js
+++ src/infrastructure/scrapers/plugins/mangadex/extract_chapters.js
@@ -1,29 +1,31 @@
 (() => {
-  const chapters = document.querySelectorAll('div.chapter.relative');
-  return Array.from(chapters).map(chapter => {
-      const infoNode = chapter.querySelector('.line-clamp-1');
+  const chapters = document.querySelectorAll("div.chapter.relative");
+  return Array.from(chapters).map((chapter) => {
+    const infoNode = chapter.querySelector(".line-clamp-1");
 
-      const card = chapter.closest('.bg-accent');
-      let headerText = '';
-      if (card) {
-          const headerNode = card.querySelector(".chapter-header .font-bold.self-center");
-          if (headerNode) headerText = headerNode.textContent;
-      }
+    const card = chapter.closest(".bg-accent");
+    let headerText = "";
+    if (card) {
+      const headerNode = card.querySelector(
+        ".chapter-header .font-bold.self-center",
+      );
+      if (headerNode) headerText = headerNode.textContent;
+    }
 
-      const anchorNode = chapter.querySelector('a[href]');
-      let href = "";
-      let language = "";
-      if (anchorNode) {
-        href = anchorNode.getAttribute('href') || '';
-        const imgNode = anchorNode.querySelector('img');
-        if (imgNode) language = imgNode.getAttribute('title') || '';
-      }
+    const anchorNode = chapter.querySelector("a[href]");
+    let href = "";
+    let language = "";
+    if (anchorNode) {
+      href = anchorNode.getAttribute("href") || "";
+      const imgNode = anchorNode.querySelector("img");
+      if (imgNode) language = imgNode.getAttribute("title") || "";
+    }
 
-      return {
-        info_text: infoNode ? infoNode.textContent : '',
-        header_text: headerText,
-        href: href,
-        language_title: language,
-      };
+    return {
+      info_text: infoNode ? infoNode.textContent : "",
+      header_text: headerText,
+      href: href,
+      language_title: language,
+    };
   });
 })();
--- src/infrastructure/scrapers/plugins/mangadex/extract_manga.js
+++ src/infrastructure/scrapers/plugins/mangadex/extract_manga.js
@@ -2,13 +2,15 @@
   const card = document.querySelector("div.layout-container.manga");
 
   const titleNode = card ? card.querySelector("div.title p") : null;
-  const name = titleNode?.textContent || '';
+  const name = titleNode?.textContent || "";
 
-  const imgNode = card ? card.querySelector("div[style*='grid-area: art'] img[alt='Cover image']") : null;
-  const thumbnail = imgNode?.getAttribute("src") || '';
+  const imgNode = card
+    ? card.querySelector("div[style*='grid-area: art'] img[alt='Cover image']")
+    : null;
+  const thumbnail = imgNode?.getAttribute("src") || "";
 
   return {
     name,
-    thumbnail
+    thumbnail,
   };
 })();

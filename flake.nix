783ca6f86da97d5c0a63ec8c97333019093af79b
Catrilao <87039539+Catrilao@users.noreply.github.com>
2026-07-01 18:26:59 -0400

    build(nix): optimize dev shell and pin stable 26.05 channel

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Manga Tracker Pipeline Environment";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
@@ -11,52 +11,25 @@
       let
         pkgs = nixpkgs.legacyPackages.${system};
 
-        commonLibraries = with pkgs; [
-          glib
-          nss
-          nspr
-          dbus
-          atk
-          cups
-          libdrm
-          libxkbcommon
-          pango
-          cairo
-          alsa-lib
-          stdenv.cc.cc.lib
-        ];
-
-        commonPythonTools = with pkgs; [
+        commonTools = with pkgs; [
+          prettier
           python312
           uv
-          ruff
-          mypy
-          python312Packages.pytest
-          python312Packages.pytest-playwright
-          python312Packages.structlog
-          python312Packages.requests
-          python312Packages.greenlet
           supabase-cli
         ];
 
         mkEnv = extraInputs:
           pkgs.mkShell {
             buildInputs =
-              commonPythonTools
-              ++ commonLibraries
+              commonTools
               ++ extraInputs;
 
-            LD_LIBRARY_PATH =
-              pkgs.lib.makeLibraryPath commonLibraries;
+            LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [ pkgs.stdenv.cc.cc.lib ];
 
             shellHook = ''
               export CHROMIUM_EXECUTABLE_PATH="${pkgs.chromium}/bin/chromium"
               export PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD=1
               export UV_LINK_MODE=copy
-
-              echo "Manga Tracker Environment"
-              echo "Python $(python3 --version | awk '{print $2}')"
-              echo "$(uv --version)"
             '';
           };
       in {
